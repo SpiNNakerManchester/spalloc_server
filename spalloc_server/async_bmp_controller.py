325f50538f6528f69b3a181a240d137528d1d1ef
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-08-01 13:59:29 +0100

    only 1 BMPConnectionData

--- spalloc_server/async_bmp_controller.py
+++ spalloc_server/async_bmp_controller.py
@@ -71,9 +71,9 @@ class AsyncBMPController(object):
         self._on_thread_start = on_thread_start
 
         self._transceiver = create_transceiver_from_hostname(
-            None, 5, bmp_connection_data=[
+            None, 5, bmp_connection_data=
                 BMPConnectionData(ip_address=hostname,
-                                  boards=[0], port_num=SCP_SCAMP_PORT)])
+                                  boards=[0], port_num=SCP_SCAMP_PORT))
         self._hostname = hostname
 
         self._stop = False
